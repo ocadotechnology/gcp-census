6d8ad7acd07ee64e453713240b901e6c71ad5979
Marcin Kołda <marcin.kolda@ocado.com>
2017-05-25 10:32:37 +0200

    updated main handler welcome message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from model.model_creator_handler import ModelCreatorHandler
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Welcome to gcp-census!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
